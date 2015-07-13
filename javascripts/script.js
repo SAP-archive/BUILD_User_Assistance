d3e14f660b48df2d00cd2964809c6cad49558ec0
Brian Murray <brian.murray@sap.com>
2015-07-13 10:42:29 +0100

    Add Help Site

---
+++ javascripts/script.js
@@ -0,0 +1,21 @@
+
+$(function() {
+    $('#showdiv1').click(function() {
+        $('div[id^=div]').hide();
+        $('#div1').show();
+    });
+    $('#showdiv2').click(function() {
+        $('div[id^=div]').hide();
+        $('#div2').show();
+    });
+
+    $('#showdiv3').click(function() {
+        $('div[id^=div]').hide();
+        $('#div3').show();
+    });
+    $('#showdiv4').click(function() {
+        $('div[id^=div]').hide();
+        $('#div4').show();
+    });
+
+})
