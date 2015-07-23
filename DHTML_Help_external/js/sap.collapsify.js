5e64d5e2966d7020fe2cd0e2be2fa3dd930b2e85
Brian Murray <brian.murray@sap.com>
2015-07-23 02:59:51 +0100

    add latest web

--- DHTML_Help_external/js/sap.collapsify.js
+++ DHTML_Help_external/js/sap.collapsify.js
@@ -1,3 +1,4 @@
+//change date  july 10, 2015
 /**
  * June 2015 Authors : i818670, i844448 Add collapsible functionality to the
  * elements when (outputclass="collapsible") is set for the elements in CSM All
@@ -12,11 +13,11 @@
 		this.$el = $(el);
 
 		this.options = {
-			imagePath : 'images/',// fix the bug for path in chm outputs
-			expandText : 'Display Content',
-			collapseText : 'Hide Content',
-			expandedIcon : 'arrowdn.gif',
-			collapsedIcon : 'arrowrt.gif'
+				imagePath : 'images/',// fix the bug for path in chm outputs
+				expandText : 'Display Content',
+				collapseText : 'Hide Content',
+				expandedIcon : 'arrowdn.gif',
+				collapsedIcon : 'arrowrt.gif'
 		};
 
 		this.opts = $.extend(this.options, opts);
@@ -59,21 +60,56 @@
 			}
 		}
 
-		if (elem.hasClass('example') && !(elem.hasClass('sap-example'))) {// example
-			// Fix for cases that text inside an example is not wrap in
-			// <p> tags
-			$('.collapsible').contents().each(function() {
+
+		if ( (elem.hasClass('example')||elem.hasClass('result')||elem.hasClass('postreq')||elem.hasClass('pre'))&& !elem.hasClass('sap-example') ){// example
+			// Fix for cases that text inside an example, result, prereq, codeblock is not wrap in <p> tag
+			// sap-example must be excluded
+			elem.contents().each(function() {
 				if (this.nodeType == 3 && $.trim(this.nodeValue) != '') {
 					$(this).wrap('<div class="textwrapped"></div>');
 				}
 			});
 		}
-		
-		var target = elem.find('.section_title, .tablecap, .figcap, .title')
-				.first();
-		target.attr('title', self.options.collapseText).wrap(div);
-		img.insertBefore(target);
+		//Adding the image icon to the elements
 
+		//sap-example  collapsible
+		if(elem.hasClass('example') && elem.hasClass('sap-example')){// sap_example dose not have a title use"Example" as hover
+			var target = elem.find('.exampletitle').first();
+			target.attr('title', self.options.collapseText).wrap(div);
+			img.insertAfter(target);
+		}
+
+		//codeblock collapsible
+		if (elem.hasClass('pre')){
+			//codeblock dose not have a title. Create fake codeblock-title to use it after the collapsible icon. use Code sample as hover
+			var target = elem;			
+			target.attr('title', self.options.collapseText).wrap(div);
+			target.wrap('<div class="codeblock-title" title="' + self.options.expandText + '">Code Sample</div>');
+			img.insertBefore(target);
+		}
+		//steps collapsible
+		if (elem.hasClass('steps') || elem.hasClass('steps-unordered')){
+			//find the "Procedure" and add the image icon before it.
+			var findProcedure = elem.siblings('.tasklabel').children('.sectiontitle ');
+			/* if (elem.hasClass('expanded')) {
+				findProcedure.addClass('collapsible expanded');
+			} else {
+				findProcedure.addClass('collapsible');
+			}
+ */
+			var target = findProcedure;		
+			target.attr('title', self.options.expandText).wrap(div);
+			img.insertBefore(target);
+		}
+
+		//all others collapsible
+		else {
+			var target = elem.find('.section_title, .tablecap, .figcap, .title, .sectiontitle, .relinfotitle')
+			.first();
+			target.attr('title', self.options.collapseText).wrap(div);
+			img.insertBefore(target);
+
+		}
 		//default is collapsed on load
 		self.toggle(elem, img);
 		// Expand element on load when triggerd by @outputclass = "collapsible expanded"
@@ -82,8 +118,8 @@
 		}
 
 	};
-	
-	// toggle visibility of children, collapsible icon
+
+	// toggle visibility of children, collapsible icon, and title hover
 	Collapsify.prototype.toggle = function(elem, img) {
 		var self = this, elemType = elem.prop('tagName');
 
@@ -93,7 +129,7 @@
 				'title' : self.options.expandText,
 				'alt' : self.options.expandText
 			}).removeClass().addClass('collapse-icon collapse');
-			elem.find('.title, .section_title, .tablecap, .figcap').first().attr('title',
+			elem.find('.title, .section_title, .tablecap, .figcap, .sectiontitle, .relinfotitle').first().attr('title',
 					self.options.expandText);
 		} else {
 			img.attr({
@@ -101,11 +137,21 @@
 				'title' : self.options.collapseText,
 				'alt' : self.options.collapseText
 			}).removeClass().addClass('collapse-icon expand');
-			elem.find('.title, .section_title, .tablecap, .figcap').first().attr('title',
+			elem.find('.title, .section_title, .tablecap, .figcap, .sectiontitle, .relinfotitle').first().attr('title',
 					self.options.collapseText);
-
 		}
-
+		if (elemType === 'PRE'){
+			// case for codeblock
+			elem.toggle();
+		}	
+		if (elemType === 'OL'){
+			// case for steps
+			elem.children().toggle();
+		}	
+		if (elemType === 'UL'){
+			// case for steps
+			elem.children().toggle();
+		}	
 		if (elemType === 'TABLE') {
 			elem.children().not('.title, caption').toggle();
 
@@ -113,15 +159,19 @@
 			if (elem.hasClass('fig')) {
 				elem.find('.image').toggle();
 			}
-
 			else if (elem.hasClass('example')) {
 				elem.children().not('.col-wrapper,.exampletitle, .title')
-						.toggle();
+				.toggle();
 			}
-
+			else if (elem.hasClass('prereq') || elem.hasClass('context') || elem.hasClass('result') || elem.hasClass('postreq')){
+				elem.children().not('.tasklabel').toggle();
+			}
+			else if (elem.hasClass('related-links')) {
+				elem.find('.extlink').toggle();
+			}
 			else {
 				elem.closest('div').children().not(
-						'.col-wrapper, .section_title').toggle();
+				'.col-wrapper, .section_title').toggle();
 			}
 		}
 
