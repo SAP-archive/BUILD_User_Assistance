8d2add2e9234715baf5c5f4a56e118840403d409
Brian Murray <brian.murray@sap.com>
2015-10-15 15:53:56 +0100

    update

--- Splash/js/search-link-format.js
+++ Splash/js/search-link-format.js
@@ -1,12 +1,5 @@
-/**
- * Creates topic links for search results list. Replace with a transtype version if 
- * you require special treatem of the @href or other attributes.
- *
- *   Copyright 2015 SAP SE.  All rights reserved
- */
- 
 function getSearchResultsHref(filename, request) {
-    return encodeURI(filename);
+    return filename;
 }
 
 function displayTopic(url) {
@@ -14,20 +7,22 @@ function displayTopic(url) {
 }
 
 function getSearchResultsLink(filename, request, linktext) {
-    //return "<a href=\"" + getSearchResultsHref(filename, request) + "\">" + linktext + "</a>"; 
-    var na = document.createElement("a");
-    na.setAttribute("href", getSearchResultsHref(filename, request));
-    na.appendChild(document.createTextNode(linktext));
-    return na;
+    return "<a href=\"" + getSearchResultsHref(filename, request) + "\">" + linktext + "</a>"; 
 }
 
 function redirect (frmSearch) {
+
     // Check browser compatibitily
     if (window.opera || navigator.userAgent.indexOf("Konquerer") > -1) {
         alert(txt_browser_not_supported);
         return;
     }
-    searchTerms = frmSearch.txtSearchTerms.value
+
+    searchTerms = document.getElementById('txtSearchTerms').value;
+    
+    if (searchTerms.length<1) {
+	 	searchTerms = document.getElementById('search-q').value;   
+	    }
     if (searchTerms.length < 1) {
         document.getElementById('results').innerHTML = "<span class=\"searchError\">" + TXT_ERR_NO_INPUT + "</span>";
     } else {
