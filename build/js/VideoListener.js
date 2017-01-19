97e66b44a4548ea5ce58555985bb872691fc669a
brian-murray35 <brian.murray@sap.com>
2017-01-19 17:41:07 +0000

    Update VideoListener.js

--- build/js/VideoListener.js
+++ build/js/VideoListener.js
@@ -13,7 +13,9 @@ var textQuote = function(text,time)
 };
 var textQuotes = [];
 var counter =0;
-textQuotes[0] = new textQuote("<p>Select <a href='BUILD_MasterDetails_video_sample_files.zip'><img style ='height:25px; width:25px;' src='download.svg'/></a>to download the sample files used in the video.</p>","01:15");
+textQuotes[0] = new textQuote("<p>Master Detail pages in BUILD prototypes are structured pages with built-in contextual-navigation. </p>","00:01");
+textQuotes[1] = new textQuote("<p>The user will be able to select and item on the List page in order to view details about the item on the Details page.</p>","00:15");
+textQuotes[2] = new textQuote("<p>Select <a href='BUILD_MasterDetails_video_sample_files.zip'><img style ='height:25px; width:25px;' src='download.svg'/></a>to download the sample files used in the video.</p>","01:20");
 var times = [];
 var x =0;
 var currentTime;
